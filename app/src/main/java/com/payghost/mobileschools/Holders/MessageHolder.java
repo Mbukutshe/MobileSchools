5995cd420e3e211c90ed35e817ece21c66a2a93d
Mbukutshe <wisemanmbukutshe@gmail.com>
2018-02-13 18:03:10 +0200

    second commits

--- app/src/main/java/com/payghost/mobileschools/Holders/MessageHolder.java
+++ app/src/main/java/com/payghost/mobileschools/Holders/MessageHolder.java
@@ -2,14 +2,23 @@ package com.payghost.mobileschools.Holders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.payghost.mobileschools.R;
 
 /**
  * Created by Wiseman on 2018-02-13.
  */
 
 public class MessageHolder extends RecyclerView.ViewHolder{
+    public TextView textViewuser,textViewdate,textViewsubject,textViewdescription;
     public MessageHolder(View view)
     {
         super(view);
+        textViewuser = view.findViewById(R.id.user);
+        textViewdate = view.findViewById(R.id.date);
+        textViewsubject = view.findViewById(R.id.subject);
+        textViewdescription = view.findViewById(R.id.description);
+
     }
 }
