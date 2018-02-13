f91cf337773b44baaf7a5bb2354b07eb10deee6d
Mbukutshe <wisemanmbukutshe@gmail.com>
2018-02-13 11:17:48 +0200

    initial commit

---
+++ app/src/main/java/com/payghost/mobileschools/Config.java
@@ -0,0 +1,8 @@
+package com.payghost.mobileschools;
+
+/**
+ * Created by Payghost on 2/13/2018.
+ */
+
+public class Config {
+}
---
+++ app/src/main/java/com/payghost/mobileschools/MainActivity.java
@@ -0,0 +1,55 @@
+package com.payghost.mobileschools;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_message:
+                    transaction.replace(R.id.content,new RetrieveMessageFrag()).commit();
+                    return true;
+                case R.id.navigation_images:
+                    transaction.replace(R.id.content,new RetrieveImagesFrag()).commit();
+                    return true;
+                case R.id.navigation_videos:
+                //    transaction.replace(R.id.content,new RtetrieveVideosFrag()).commit();
+                    return true;
+                case R.id.navigation_documents:
+                //    transaction.replace(R.id.content,new RetrieveDocumentsFrag()).commit();
+                    return true;
+                case R.id.navigation_more:
+                 //   transaction.replace(R.id.content,new MoreFrag()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        transaction.replace(R.id.content,new RetrieveMessageFrag()).commit();
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
